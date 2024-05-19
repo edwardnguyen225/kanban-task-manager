b8cbd155359fec15832fa619bf7d9f1def08568c
Nhan Nguyen <48123439+edwardnguyen225@users.noreply.github.com>
2024-05-20 02:59:45 +0700

    feat: Add DatabaseModule and DatabaseService to app.module.ts
    This commit adds the `DatabaseModule` and `DatabaseService` to the `app.module.ts` file. The `DatabaseModule` is imported and added to the `imports` array, along with the existing `UsersModule`. The `DatabaseService` is injected into the `UsersService` constructor. This change sets up the foundation for interacting with the database in the application.

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
+import { DatabaseService } from 'src/database/database.service';
+import bcrypt from 'bcrypt';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private databaseService: DatabaseService) {}
+
+  async findOne(email: string): Promise<User> {
+    const user = await this.databaseService.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new NotFoundException();
+    }
+
+    delete user.password;
+    return user;
+  }
+
+  async create(data: Prisma.UserCreateInput): Promise<User> {
+    const existing = await this.databaseService.user.findUnique({
+      where: { email: data.email },
+    });
+
+    if (existing) {
+      throw new ConflictException('username_already_exists');
+    }
+
+    // the second argument ( 10 ) is just a "cost factor".
+    // the higher the cost factor, the more difficult is brute-forcing
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    const user = await this.databaseService.user.create({
+      data: {
+        ...data,
+        password: hashedPassword,
+      },
+    });
+
+    delete user.password;
+    return user;
+  }
+}
